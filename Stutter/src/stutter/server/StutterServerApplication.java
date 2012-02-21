9f8c9590f7aaff5e2b369da03d50b7a87fdd78fc
Paul Heaberlin <paul@heaberlin.net>
2012-02-21 12:21:33 -0600

    Add comments indicating the mapping of URI to service

--- Stutter/src/stutter/server/StutterServerApplication.java
+++ Stutter/src/stutter/server/StutterServerApplication.java
@@ -8,7 +8,7 @@ import org.restlet.routing.Router;
 
 public class StutterServerApplication extends Application {
 
-    // TODO: can this go away?
+    // this isn't needed when deploying within proper environment (Tomcat, etc), I'm just lazy
     private static final String BASE = "http://localhost:8182";
 
     public static void main(String[] args) throws Exception {
@@ -23,10 +23,14 @@ public class StutterServerApplication extends Application {
 
         router.attach(BASE + "/", RootServerResource.class);
 
+        // for the User list or to add a new User, use the UserList service
         router.attach(BASE + "/users", UserListServerResource.class);
+        // for a specific User (to view, update, or delete), use the User service
         router.attach(BASE + "/users/{username}", UserServerResource.class);
 
+        // for the Mumble list or to add a new Mumble, use the MumbleList service
         router.attach(BASE + "/mumbles", MumbleListServerResource.class);
+        // for a specific Mumble (to view, or delete), use the Mumble service
         router.attach(BASE + "/mumbles/{mumbleId}", MumbleServerResource.class);
 
         return router;
